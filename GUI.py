85771118355bd201277c188638977ec3d98d0596
민Hieu <53557912+min-hieu@users.noreply.github.com>
2020-03-15 18:28:02 +0900

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,291 @@
+# -*- coding: utf-8 -*-
+# Author: Nguyen Minh Hieu
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import pyqtSlot
+from matplotlib.backends.qt_compat import QtCore, QtWidgets
+from matplotlib.backends.backend_qt5agg import (FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+from matplotlib.figure import Figure
+from supernew import superSeq
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        self.MainWindow = MainWindow
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(900, 600)
+        MainWindow.setMinimumSize(QtCore.QSize(900, 600))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        # self.widgetGraph = QtWidgets.QWidget(self.centralwidget)
+        # self.widgetGraph.setMinimumSize(QtCore.QSize(560, 0))
+        # self.widgetGraph.setMaximumSize(QtCore.QSize(560, 541))
+        # self.widgetGraph.setObjectName("widgetGraph")
+
+        # self.horizontalLayout.addWidget(self.widgetGraph)
+        
+        #======== Add Canvas Widget ===========
+        canvasGraph = FigureCanvas(Figure(figsize=(5, 3)))
+        self.horizontalLayout.addWidget(canvasGraph)
+        MainWindow.addToolBar(NavigationToolbar(canvasGraph, MainWindow))
+
+        MainWindow.ax = canvasGraph.figure.subplots()
+        MainWindow.ax.text(.2,.5, "HAPPY SUPERCHARGING! - Hieu")
+
+
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.formLayout_2 = QtWidgets.QFormLayout()
+        self.formLayout_2.setObjectName("formLayout_2")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setContentsMargins(0, -1, -1, 0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        spacerItem = QtWidgets.QSpacerItem(20, 80, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        self.verticalLayout_2.addItem(spacerItem)
+        self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_4.setContentsMargins(-1, 0, -1, -1)
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        spacerItem1 = QtWidgets.QSpacerItem(5, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_4.addItem(spacerItem1)
+        self.labelSeq = QtWidgets.QLabel(self.centralwidget)
+        self.labelSeq.setObjectName("labelSeq")
+        self.horizontalLayout_4.addWidget(self.labelSeq)
+        spacerItem2 = QtWidgets.QSpacerItem(5, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_4.addItem(spacerItem2)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_4)
+        self.horizontalLayout_7 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_7.setContentsMargins(-1, 0, -1, -1)
+        self.horizontalLayout_7.setObjectName("horizontalLayout_7")
+        spacerItem3 = QtWidgets.QSpacerItem(5, 20, QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_7.addItem(spacerItem3)
+        self.buttonGraph = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonGraph.setMaximumSize(QtCore.QSize(50, 16777215))
+        self.buttonGraph.setObjectName("buttonGraph")
+        self.horizontalLayout_7.addWidget(self.buttonGraph)
+        spacerItem4 = QtWidgets.QSpacerItem(5, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_7.addItem(spacerItem4)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_7)
+        spacerItem5 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_2.addItem(spacerItem5)
+        self.formLayout_2.setLayout(0, QtWidgets.QFormLayout.LabelRole, self.verticalLayout_2)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.inputSeq = QtWidgets.QTextEdit(self.centralwidget)
+        self.inputSeq.setMinimumSize(QtCore.QSize(0, 0))
+        self.inputSeq.setMaximumSize(QtCore.QSize(16777215, 16777215))
+        self.inputSeq.setObjectName("inputSeq")
+        self.horizontalLayout_2.addWidget(self.inputSeq)
+        self.buttonImportSeq = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonImportSeq.setText("")
+        self.buttonImportSeq.setObjectName("buttonImportSeq")
+        self.horizontalLayout_2.addWidget(self.buttonImportSeq)
+        self.formLayout_2.setLayout(0, QtWidgets.QFormLayout.FieldRole, self.horizontalLayout_2)
+        self.labelThres = QtWidgets.QLabel(self.centralwidget)
+        self.labelThres.setObjectName("labelThres")
+        self.formLayout_2.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.labelThres)
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.inputThres = QtWidgets.QSpinBox(self.centralwidget)
+        self.inputThres.setMaximumSize(QtCore.QSize(16777215, 23))
+        self.inputThres.setObjectName("inputThres")
+        self.horizontalLayout_3.addWidget(self.inputThres)
+        spacerItem6 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem6)
+        self.formLayout_2.setLayout(1, QtWidgets.QFormLayout.FieldRole, self.horizontalLayout_3)
+        self.labelTarget = QtWidgets.QLabel(self.centralwidget)
+        self.labelTarget.setObjectName("labelTarget")
+        self.formLayout_2.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.labelTarget)
+        self.inputTarget = QtWidgets.QTextEdit(self.centralwidget)
+        self.inputTarget.setMaximumSize(QtCore.QSize(16777215, 23))
+        self.inputTarget.setObjectName("inputTarget")
+        self.formLayout_2.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.inputTarget)
+        self.labelStandin = QtWidgets.QLabel(self.centralwidget)
+        self.labelStandin.setObjectName("labelStandin")
+        self.formLayout_2.setWidget(3, QtWidgets.QFormLayout.LabelRole, self.labelStandin)
+        self.inputStandin = QtWidgets.QTextEdit(self.centralwidget)
+        self.inputStandin.setMaximumSize(QtCore.QSize(16777215, 23))
+        self.inputStandin.setObjectName("inputStandin")
+        self.formLayout_2.setWidget(3, QtWidgets.QFormLayout.FieldRole, self.inputStandin)
+        self.labelBinding = QtWidgets.QLabel(self.centralwidget)
+        self.labelBinding.setObjectName("labelBinding")
+        self.formLayout_2.setWidget(4, QtWidgets.QFormLayout.LabelRole, self.labelBinding)
+        self.inputBinding = QtWidgets.QTextEdit(self.centralwidget)
+        self.inputBinding.setMaximumSize(QtCore.QSize(16777215, 46))
+        self.inputBinding.setObjectName("inputBinding")
+        self.formLayout_2.setWidget(4, QtWidgets.QFormLayout.FieldRole, self.inputBinding)
+        self.labelConsurf = QtWidgets.QLabel(self.centralwidget)
+        self.labelConsurf.setObjectName("labelConsurf")
+        self.formLayout_2.setWidget(5, QtWidgets.QFormLayout.LabelRole, self.labelConsurf)
+        self.horizontalLayout_6 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_6.setObjectName("horizontalLayout_6")
+        self.labelConsurfStatus = QtWidgets.QLabel(self.centralwidget)
+        self.labelConsurfStatus.setObjectName("labelConsurfStatus")
+        self.horizontalLayout_6.addWidget(self.labelConsurfStatus)
+        self.buttonImportConsurf = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonImportConsurf.setObjectName("buttonImportConsurf")
+        self.horizontalLayout_6.addWidget(self.buttonImportConsurf)
+        spacerItem7 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_6.addItem(spacerItem7)
+        self.formLayout_2.setLayout(5, QtWidgets.QFormLayout.FieldRole, self.horizontalLayout_6)
+        self.verticalLayout.addLayout(self.formLayout_2)
+        spacerItem8 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem8)
+        self.horizontalLayout_5 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_5.setContentsMargins(-1, 0, -1, -1)
+        self.horizontalLayout_5.setObjectName("horizontalLayout_5")
+        spacerItem9 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_5.addItem(spacerItem9)
+        self.checkAdd = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkAdd.setObjectName("checkAdd")
+        self.horizontalLayout_5.addWidget(self.checkAdd)
+        self.buttonSupercharge = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonSupercharge.setObjectName("buttonSupercharge")
+        self.horizontalLayout_5.addWidget(self.buttonSupercharge)
+        spacerItem10 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_5.addItem(spacerItem10)
+        self.verticalLayout.addLayout(self.horizontalLayout_5)
+        self.horizontalLayout_8 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_8.setContentsMargins(-1, 0, -1, -1)
+        self.horizontalLayout_8.setObjectName("horizontalLayout_8")
+        spacerItem11 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_8.addItem(spacerItem11)
+        self.buttonClear = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonClear.setObjectName("buttonClear")
+        self.horizontalLayout_8.addWidget(self.buttonClear)
+        spacerItem12 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_8.addItem(spacerItem12)
+        self.verticalLayout.addLayout(self.horizontalLayout_8)
+        spacerItem13 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem13)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 900, 21))
+        self.menubar.setObjectName("menubar")
+        self.menuHelp = QtWidgets.QMenu(self.menubar)
+        self.menuHelp.setObjectName("menuHelp")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionImport_Fasta = QtWidgets.QAction(MainWindow)
+        self.actionImport_Fasta.setObjectName("actionImport_Fasta")
+        self.actionExport = QtWidgets.QAction(MainWindow)
+        self.actionExport.setObjectName("actionExport")
+        self.menuHelp.addAction(self.actionImport_Fasta)
+        self.menuHelp.addAction(self.actionExport)
+        self.menubar.addAction(self.menuHelp.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+
+        #============== Connecting to Backend ==================
+        self.buttonImportSeq.clicked.connect(lambda: self.importSeq())
+
+        self.buttonImportConsurf.clicked.connect(lambda: self.importConsurf())
+
+        self.buttonSupercharge.clicked.connect(lambda: self.superCharge(MainWindow.ax))
+        self.consurfScore = None
+        
+        self.buttonClear.clicked.connect(lambda: self.clearGraph(MainWindow.ax))
+
+        self.buttonGraph.clicked.connect(lambda: self.drawGraph(MainWindow.ax))
+
+        #=============== Testing ================
+        self.inputSeq.setText('''MLPGVGLTPS AAQTARQHPK MHLAHSTLKP AAHLIGDPSK QNSLLWRANT
+        DRAFLQDGFS LSNNSLLVPT SGIYFVYSQV VFSGKAYSPK ATSSPLYLAH
+        EVQLFSSQYP FHVPLLSSQK MVYPGLQEPW LHSMYHGAAF QLTQGDQLST
+        HTDGIPHLVL SPSTVFFGAF AL''')
+        self.inputBinding.setText('20-23+27')
+        self.inputTarget.setText("R, K")
+        self.inputStandin.setText("D, E")
+
+
+    @pyqtSlot()
+    def importSeq(self):
+        filename = QtWidgets.QFileDialog.getOpenFileName(None, 'Select Sequence File', '.txt')
+        file = open(list(filename)[0], 'r')
+
+        with file:
+            text = file.read()
+            self.inputSeq.setText(text)
+
+
+    @pyqtSlot()
+    def importConsurf(self):
+        filename = QtWidgets.QFileDialog.getOpenFileName(None, 'Select Consurf File', '.txt')
+        
+        self.consurfScore = superSeq.consurfReader(list(filename)[0])
+        self.labelConsurfStatus.setText("Imported")
+
+
+    @pyqtSlot()
+    def superCharge(self,ax):
+        thres = int(self.inputThres.text())
+        target = self.inputTarget.toPlainText()
+        standin = self.inputStandin.toPlainText()
+        bSite = self.inputBinding.toPlainText()
+        sSeq = superSeq(self.inputSeq.toPlainText(),bindingSite=bSite)
+
+        
+        x, y1, y2 = sSeq.superGraph(thres=thres,no=sSeq.bindingSite)
+        if not self.checkAdd.isChecked():
+            ax.clear()
+            ax.plot(x, y1, color='blue')
+            ax.plot(x, y2, color='orange')
+            ax.axhline(thres, 0, 1, alpha=0.6, color="#00F481")
+        else:
+            ax.plot(x,y2)
+    
+        ax.figure.canvas.draw()
+        print('pressed')
+
+    @pyqtSlot()
+    def clearGraph(self, ax):
+        ax.clear()
+        ax.figure.canvas.draw()
+
+    @pyqtSlot()
+    def drawGraph(self,ax):
+        bSite = self.inputBinding.toPlainText()
+        sSeq = superSeq(self.inputSeq.toPlainText(),bindingSite=bSite)
+
+        ax.clear()
+        y = sSeq.getChargePool(sSeq.seqStr)
+        ax.plot(range(len(y)),y,color='purple')
+
+        ax.figure.canvas.draw()
+
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "SuperCharging Automation"))
+        self.labelSeq.setText(_translate("MainWindow", "Sequence"))
+        self.buttonGraph.setText(_translate("MainWindow", "Graph"))
+        self.labelThres.setText(_translate("MainWindow", "Threshold"))
+        self.labelTarget.setText(_translate("MainWindow", "Target AA"))
+        self.labelStandin.setText(_translate("MainWindow", "Stand-in AA"))
+        self.labelBinding.setText(_translate("MainWindow", "Binding site"))
+        self.labelConsurf.setText(_translate("MainWindow", "ConSurf"))
+        self.labelConsurfStatus.setText(_translate("MainWindow", "Not Imported"))
+        self.buttonImportConsurf.setText(_translate("MainWindow", "Import"))
+        self.checkAdd.setText(_translate("MainWindow", "add"))
+        self.buttonSupercharge.setText(_translate("MainWindow", "Supercharge"))
+        self.buttonClear.setText(_translate("MainWindow", "clear"))
+        self.menuHelp.setTitle(_translate("MainWindow", "File"))
+        self.actionImport_Fasta.setText(_translate("MainWindow", "Import"))
+        self.actionExport.setText(_translate("MainWindow", "Export"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
---
+++ supernew.py
@@ -0,0 +1,241 @@
+# -*- coding: utf-8 -*-
+# Author: Nguyen Minh Hieu
+
+
+
+import math
+import bisect
+import random
+
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib import style
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#-------------Supercharging Standalone Backend--------------
+
+#Global variables
+chargeDict={'D': -1, 'T': 0, 'S': 0, 'E': -1, 'P': 0, 'G': 0, 'A': 0,\
+            'C': -0.1, 'V': 0, 'M': 0, 'I': 0, 'L': 0, 'Y': 0,'F': 0,\
+            'H': 0.1, 'K': 1, 'R': 1, 'W': 0, 'Q': 0, 'N': 0}
+
+aaList=['D', 'T', 'S', 'E', 'P', 'G', 'A', 'C', 'V', 'M',
+    'I', 'L', 'Y', 'F', 'H', 'K', 'R', 'W', 'Q', 'N']
+
+style.use("seaborn-notebook")
+
+
+
+class superSeq(object):
+
+    def __init__(self, seqRaw, filename='', name='protein',bindingSite=None,formated=False):
+        if formated==False:
+            self.seqStr = self.formatStr(seqRaw)
+            self.seq = self.formatSeq(seqRaw)
+        else:
+            self.seqStr = seqRaw
+            self.seq = list(seqRaw)
+        self.bindingSite = self.formatBindingSite(bindingSite)
+        self.name = name
+        self.location = filename
+
+    def __str__(self):
+        return self.seqStr
+
+    def __eq__(self, sSeq_):
+        assert type(sSeq_) == superSeq
+        return self.seqStr == sSeq_.seqStr
+
+    def __len__(self):
+        return len(self.seqStr)
+
+    def __add__(self, sSeq_, name_=''):
+        assert type(sSeq_) == superSeq
+        newSeqRaw = self.seqStr + sSeq_.seqStr
+        self = superSeq(newSeqRaw,name='',formated=True)
+
+    def showLCD(self):
+        charge = self.getChargePool(self.seqStr)
+
+        x = range(len(charge))
+        y = charge
+
+        plt.plot(x, y)
+        plt.axhline(4, 0, 1, alpha=0.6, color="orange")
+        plt.xlabel("Window Index")
+        plt.ylabel("Charge Index")
+        plt.suptitle(self.name, fontsize=20)
+
+        plt.show()
+
+    def compareLCD(self, sSeq, threshold=4):
+        assert type(sSeq) == superSeq
+        x = range(len(self.getChargePool(self.seqStr)))
+        y1 = sSeq.getCharge(self.seqStr)
+        y2 = sSeq.getCharge(sSeq.seqStr)
+
+        title = self.name + " normal(blue) vs charged(orange)"
+
+        plt.plot(x, y1, color='blue')
+        plt.plot(x, y2, color='orange')
+        plt.axhline(threshold, 0, 1, alpha=0.6, color="#00F481")
+        plt.xlabel("Window Index")
+        plt.ylabel("Charge Index")
+        plt.suptitle(title, fontsize=15)
+
+        plt.show()
+
+    def compareSeq(self, sSeq):
+        assert len(self) == len(sSeq)
+        diff_arr = []
+
+        for i, aa in enumerate(sSeq.seq):
+            pass
+    
+    @staticmethod
+    def formatStr(seqRaw):
+        assert type(seqRaw) == str
+        seqStr = ''
+        seqRaw = seqRaw.upper()
+        
+        for i in seqRaw:
+            if i in aaList:
+                seqStr += i
+
+        return seqStr
+
+    #format the string and remove characters that are not aminoacid
+    @staticmethod
+    def formatSeq(seqRaw):
+        assert type(seqRaw) == str
+        seqArray = []
+        seqRaw = seqRaw.upper()
+        
+        for i in seqRaw:
+            if i in aaList:
+                seqArray.append(i)
+
+        return seqArray
+                    
+    @staticmethod
+    def getChargeArray(sStr):
+        return [chargeDict[i] for i in sStr]
+
+    #return the sequence which yield the charge of the input superSeq object
+    #w stands for window size of the input
+    @staticmethod
+    def getChargePool(sStr, w=20):
+        chargeArray = []
+
+        for i in range(len(sStr)-(w-1)):
+            chargeArray.append(round(sum([chargeDict[j] for j in sStr[i:i+w]]),1))
+
+        return chargeArray
+
+    @staticmethod
+    def chargeArraytoPool(chArray, w=20):
+        chargeArray = []
+
+        for i in range(len(chArray)-w+1):
+            chargeArray.append(round(sum([j for j in chArray[i:i+w]]),1))
+
+        return chargeArray
+
+    @staticmethod
+    def getChargeSingle(seq,formated=True):
+        if formated==False:
+            seq = superSeq.formatStr(seq)
+
+        return sum(round([chargeDict[i] for i in seq],1))
+
+    
+    @staticmethod
+    def consurfReader(filename):
+        assert type(filename) == str
+        if filename == "":
+            return None
+
+        score = []
+
+        with open(filename, "r") as f:
+            for i, line in enumerate(f):
+                if i > 14:
+                    score.append(line.split()[4])
+
+        return score
+
+    @staticmethod
+    def formatBindingSite(site):
+        if site == None or site == '': return None
+        # format: #1-#2+#3 (from #1 to #2 add #3)
+        # output (tuple): (#1,...,#2,#3)
+
+        siteList = site.split('+')
+        newList = []
+
+        for i in siteList:
+            try: newList.append(int(i))
+            except ValueError:
+                op,ed = map(int, i.split('-'))
+                for j in range(op,ed+1):
+                    newList.append(j)
+            
+
+        return tuple(newList)
+    
+
+    def getSuperExhaustive(self, threshold=4, limit=-1, window=20, noMutate=[],target=['R','K'],standin=['D','E']):
+        charge = superSeq.getChargeArray(self.seqStr)
+        aaseq = list(self.seqStr[:])
+        n = len(charge)
+
+        mutated = []
+
+        for i in range(n-window):
+            if round(sum(charge[i:i+window]),1) > threshold:
+                for j in range(20)[::-1]:
+                    if aaseq[i+j] in target:
+                        charge[i+j] = -1
+                        aaseq[i+j] = random.choice(standin)
+                        mutated.append(i+j)
+                    if round(sum(charge[i:i+window]),1) < threshold:
+                        break
+                        
+        return mutated, charge
+
+
+    def superGraph(self,thres=4,limit=-1,w=20,no=[],tar=['R','K'],stand=['D','E']):
+
+        y1 = self.getChargePool(self.seqStr)
+        
+        mutated,y2 = self.getSuperExhaustive(threshold=thres,window=w,noMutate=no,target=tar,standin=stand)
+
+        y2 = self.chargeArraytoPool(y2)
+
+        x = range(len(y1))
+        print(mutated)
+
+        return x,y1,y2
+
+
+#----------------Unit Testing---------------
+
+def test(sSeq):
+    sSeq = superSeq(sSeq)
+    charge = superSeq.getChargePool(sSeq.seqStr)
+
+    x = range(len(charge))
+    y = charge
+
+    print(x, y)
+
+# test("MLPGVGLTPS AAQTARQHPK MHLAHSTLKP AAHLIGDPSK QNSLLWRANT\
+#         DRAFLQDGFS LSNNSLLVPT SGIYFVYSQV VFSGKAYSPK ATSSPLYLAH\
+#         EVQLFSSQYP FHVPLLSSQK MVYPGLQEPW LHSMYHGAAF QLTQGDQLST\
+#         HTDGIPHLVL SPSTVFFGAF AL")
